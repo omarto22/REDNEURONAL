bb39f8c70148a89ccaf77c96234e06cf4b46514c
omarto22 <omtoga22@gmail.com>
2024-11-23 12:41:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import pandas as pd
 
 st.title("Credit Card App")
 
+    
 # ============ Aplicación Principal  ============
         
 # Definir las opciones de página
 pages = ["Cargar Datos", "Explorar Datos", "Feature Engineering", "Modelado", "Neural Network", "Prediccion"]
+
 
 # Mostrar un menú para seleccionar la página
 selected_page = st.sidebar.multiselect("Seleccione una página", pages)
@@ -25,29 +27,31 @@ if "Cargar Datos" in selected_page:
         dataset = pd.read_csv(uploaded_file)
     # Mostrar datos en una tabla
         st.write(dataset)
-        
 
 if "Explorar Datos" in selected_page:
     st.write("""
     ## Explore Data
     Distributions""")
-
+        
 if "Feature Engineering" in selected_page:
     st.write("""
-    ## Explore Data
-    Feature Engineering""")
+    ## Feature Engineering
+    New datset""")
 
 if "Modelado" in selected_page:
     st.write("""
-    ## Explore Data
-    Modelado""")
+    ## Entrenamiento con diferentes modelos
+    Resultados""")
 
+        
 if "Neural Network" in selected_page:
     st.write("""
-    ## Explore Data
-    Neural Network""")
-       
+    ## Neural Network
+    Resultados""")
+
+        
 if "Prediccion" in selected_page:
     st.write("""
-    ## Explore Data
-    Prediccion""")
+    ## Predicción de un Crédito
+    Capture los datos""")
+ 
